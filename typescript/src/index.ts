464b863e1ae93dc186dd286f42633d2094a05173
Rizqi Reza Ardiansyah <rizqirezaardiansyah@gmail.com>
2024-07-24 10:24:15 +0700

    Initial Commit

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -10,4 +10,41 @@ console.log(numbers)
 const isMarried: boolean = true
 console.log(isMarried);
 
+// Eksplisit
+let firstName: string = "Reza"
+console.log(firstName);
+
+firstName = "Rizqi"
+console.log(firstName);
+
+// Implisit
+const address: string = "Tangerang"
+
+// Type Alias 
+// seperti di bahasa golang untuk membuat sebuah struct atau interface
+type Student = {
+    id: string
+    name: string
+}
+
+const fulan: Student = {
+    id: "1",
+    name: "Ardiansyah"
+}
+
+console.log(fulan);
+
+// Enum
+// Bisa digunakan untu membuat sebuah method get, post, update, delete dan statusCode
+// Secara default enum ini akan mengisi dimulai dari 0
+enum MyEnum {
+    ChoiceA,
+    ChoiceB,
+    ChoiceC = "C"
+}
+
+console.log(MyEnum.ChoiceC);
+
+
+
 
