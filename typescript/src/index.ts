3cd25a4b6193236cdba692d013cdc45484ea52f4
Rizqi Reza Ardiansyah <rizqirezaardiansyah@gmail.com>
2024-07-24 10:11:50 +0700

    Initial Commit

---
+++ typescript/src/index.ts
@@ -0,0 +1,13 @@
+// Variable
+// ES6 -> const (tidak bisa diubah / tidak bisa reassign)
+
+const fullName: string = "Rizqi Reza Ardiansyah"
+console.log(fullName)
+
+const numbers: number[] = [1,2,3,4,5]
+console.log(numbers)
+
+const isMarried: boolean = true
+console.log(isMarried);
+
+
